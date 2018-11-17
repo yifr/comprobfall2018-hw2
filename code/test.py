b8fd6fa6a2c98dd62c6d468b5935fcea2f1a4ae6
lawrencelin97 <lawrencelin97@gmail.com>
2018-11-17 13:38:46 -0500

    updated piano mover

--- code/test.py
+++ code/test.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 Created on Fri Nov 16 19:44:25 2018
@@ -5,20 +6,35 @@ Created on Fri Nov 16 19:44:25 2018
 @author: lawrence
 """
 
-from gazebo_msgs.msg import ModelState
-from gazebo_msgs.msg import Gazebo.srv
+import rospy
 
-pub = rospy.Publisher('/gazebo/set_model_state', model_state, queue_size=10)
+from gazebo_msgs.msg import ModelState
+from geometry_msgs.msg import Twist
+
+
+rospy.init_node('hello')
 
 def move_model(model_name, pose, quaternion):
-	model_state = GazeboState()
-	model_state.position.x = pose[0]
-	model_state.position.y = pose[1]
-	model_state.position.z = pose[2]
+	pub = rospy.Publisher('/gazebo/set_model_state', ModelState, queue_size=10)
+	
+	model_state = ModelState()
 
-	model_state.orientation.x = quaternion[0]
-	model_state.orientation.x = quaternion[1]
-	model_state.orientation.x = quaternion[2]
-	model_state.orientation.x = quaternion[3]
+	model_state.model_name='piano2'
+	model_state.pose.position.x = 10
+	model_state.pose.position.y = 6
+	model_state.pose.position.z = 5
+	model_state.pose.orientation.x = 0
+	model_state.pose.orientation.y = 1
+	model_state.pose.orientation.z = 2
+	model_state.pose.orientation.w = 3
+	#print('hello')
+	model_state.reference_frame=""
+	w = Twist()
+	model_state.twist = w
+	rate = rospy.Rate(10)
+	while not rospy.is_shutdown():
+		pub.publish(model_state)
+		rate.sleep()
 
-	pub.publish(model_state)
+move_model("piano2",[10,6,5],[0,0,0,0])
+
